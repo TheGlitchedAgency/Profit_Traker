beaf486ea903b95a4193143b5d44de740af3d974
Glitch <cmason102008@gmail.com>
2026-03-31 09:34:23 -0400

    stress test

--- Web/js/profit.js
+++ Web/js/profit.js
@@ -1,6 +1,6 @@
 /* Profit Summary Application */
 
-const API_BASE = "https://ing-speakers-invoice-vbulletin.trycloudflare.com/api/profits";
+const API_BASE = "https://political-wallace-studying-monster.trycloudflare.com/api/profits";
 
 
 
